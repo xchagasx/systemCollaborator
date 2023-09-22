2e9bdadf56a15617d10ac9ef07c6b2fc5175934d
FernandoChagas <git@github.com:xchagasx/node-boleto.git>
2023-09-21 23:01:14 -0300

    finish day

--- systemcollaboratorlogin/src/app.js
+++ systemcollaboratorlogin/src/app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
+import RoutesApp from "./routes";
 
 const App = () => {
   return (
     <>
-      <div>App</div>
+      <RoutesApp />
       <GlobalStyle />
     </>
   )
